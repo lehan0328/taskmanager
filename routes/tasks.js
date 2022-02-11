a8cebc91c32cc7766f943f328f40490f9811dc27
lehan ouyang <lehanouyang@lehans-MacBook-Pro.local>
2022-02-11 18:10:57 -0500

    update

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router();
-const{getAllTasks,createTask,getTask,updateTask,deleteTask} = require("../constrollers/tasks")
+const{getAllTasks,createTask,getTask,updateTask,deleteTask} = require("../controllers/tasks")
 router.route('/').get(getAllTasks).post(createTask)
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 module.exports = router
